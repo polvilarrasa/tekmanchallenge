ccefd58c650fd1ea58331a8d404c15d319df4886
Pol Vilarrasa <pol.vilarrasa@estudiants.urv.cat>
2022-10-21 22:04:47 +0200

    configurar doctrine i nginx

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/orders', name: 'get_orders', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        die("working");
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/DefaultController.php',
+        ]);
+    }
+}
